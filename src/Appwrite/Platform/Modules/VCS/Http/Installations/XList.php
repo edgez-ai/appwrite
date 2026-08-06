d4e01285b4acda956a55dccb0243d1a8e96e88f7
Jason Hao <jasonhao518@gmail.com>
2026-08-06 16:22:40 +0200

    Accept VCS cross-project query boolean

--- src/Appwrite/Platform/Modules/VCS/Http/Installations/XList.php
+++ src/Appwrite/Platform/Modules/VCS/Http/Installations/XList.php
@@ -53,7 +53,7 @@ class XList extends Action
             ->param('queries', [], new Installations(), 'Array of query strings generated using the Query class provided by the SDK. [Learn more about queries](https://appwrite.io/docs/queries). Maximum of ' . APP_LIMIT_ARRAY_PARAMS_SIZE . ' queries are allowed, each ' . APP_LIMIT_ARRAY_ELEMENT_SIZE . ' characters long. You may filter on the following attributes: ' . implode(', ', Installations::ALLOWED_ATTRIBUTES), true)
             ->param('search', '', new Text(256), 'Search term to filter your list results. Max length: 256 chars.', true)
             ->param('total', true, new Boolean(true), 'When set to false, the total count returned will be 0 and will not be calculated.', true)
-            ->param('includeAllProjects', false, new Boolean(), 'Include installations from other accessible projects.', true)
+            ->param('includeAllProjects', false, new Boolean(true), 'Include installations from other accessible projects.', true)
             ->inject('response')
             ->inject('project')
             ->inject('dbForPlatform')
